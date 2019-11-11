113bd85b9f735f69c0409de2e11c8c86f0485490
Martin Graham <martinjamesgraham@outlook.com>
2019-11-11 11:26:43 +0000

    Add recaptcha check to gothub post php side

--- php/githubIssue.php
+++ php/githubIssue.php
@@ -2,6 +2,10 @@
     session_start();
     include('utils.php');
 
+    // validate captcha
+    $captcha = validatePostVar ("g-recaptcha-response", '/.{1,}/', false, "recaptchaWidget");
+    validateCaptcha ($captcha);
+
     if (isset($_POST["issueName"]) && isset($_POST["issueText"])) {
         $json = sendGithubIssue ($_POST["issueName"], $_POST["issueText"]);
         echo ($json);
